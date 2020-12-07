4de61faadc55a42710bf7100cdf3b126de9ae441
izzudin26 <azidarmor26@gmail.com>
2020-12-08 04:56:46 +0700

    fixing problem async await

--- backend/src/controller/IstilahController.ts
+++ backend/src/controller/IstilahController.ts
@@ -1,34 +1,54 @@
-import {istilahScrap} from "../Scrapper/IstilahScrapper"
-import {Request, Response} from "express"
+import { istilahScrap } from "../Scrapper/IstilahScrapper";
+import { Request, Response } from "express";
 
 interface IResponseApi {
-    status: number
-    message: string
-    data: any
+  status: number;
+  message: string;
+  data: any;
 }
 
 class IstilahController {
-    public getKbbi(req: Request, res: Response): Response {
-        try {
-            let kbbiData = istilahScrap.findKbbi(req.body.keyword)
-            let response: IResponseApi = {status: 200, message: "Success", data: kbbiData}
-            return res.send(response).status(200)
-        } catch (error) {
-            let response: IResponseApi = {status: 500,message: error, data: null}
-            return res.send(response).status(500)
-        }
-    }
+  public async getKbbi(req: Request, res: Response): Promise<Response> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let kbbiData = await istilahScrap.findKbbi(req.body.keyword);
+        let response: IResponseApi = {
+          status: 200,
+          message: "Success",
+          data: kbbiData,
+        };
+        return resolve(res.send(response).status(200));
+      } catch (error) {
+        let response: IResponseApi = {
+          status: 500,
+          message: error,
+          data: null,
+        };
+        return reject(res.send(response).status(500));
+      }
+    });
+  }
 
-    public getWiki(req: Request, res: Response): Response {
-        try {
-            let wikiData = istilahScrap.findWikipedia(req.body.keyword)
-            let response: IResponseApi = {status: 200, message: "Success", data: wikiData}
-            return res.send(response).status(200)
-        } catch (error) {
-            let response: IResponseApi = {status: 500, message: error, data: null}
-            return res.send(response).status(500)
-        }
-    }
+  public async getWiki(req: Request, res: Response): Promise<Response> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let wikiData = await istilahScrap.findWikipedia(req.body.keyword);
+        let response: IResponseApi = {
+          status: 200,
+          message: "Success",
+          data: wikiData,
+        };
+        return resolve(res.send(response).status(200));
+      } catch (error) {
+        let response: IResponseApi = {
+          status: 500,
+          message: error,
+          data: null,
+        };
+        return reject(res.send(response).status(500));
+      }
+    });
+  }
 }
 
 export const istilahController = new IstilahController();
