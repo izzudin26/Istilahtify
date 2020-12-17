ea21cbeb1e37fa4d46497891cc230e11f7152787
izzudin26 <azidarmor26@gmail.com>
2020-12-17 19:17:57 +0700

    complete body component

--- frontend/src/components/body.vue
+++ frontend/src/components/body.vue
@@ -1,23 +1,66 @@
 <template>
   <div>
-    <v-col class="mx-auto" md="6" sm="12" lg="6">
-      <v-text-field
-        solo
-        class="keyword-field"
-        label="Kata Kunci"
-        append-icon="mdi-magnify"
-        @click:append="callIstilah"
-      >
-      </v-text-field>
+    <v-col class="mx-auto" md="8" sm="12" lg="6">
+      <v-form ref="form" v-model="isValid" lazy-validation>
+        <v-text-field
+          solo
+          v-model="keyword"
+          class="keyword-field"
+          label="Kata Kunci"
+          append-icon="mdi-magnify"
+          @click:append="callIstilah()"
+          :loading="isProcess"
+          :rules="keywordRules"
+        >
+        </v-text-field>
+      </v-form>
     </v-col>
+    <v-container>
+      <v-tabs centered>
+        <v-tab>Wikipedia</v-tab>
+        <v-tab>KBBI</v-tab>
+      </v-tabs>
+    </v-container>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { istilahService } from "@/WebServices/istilahServices.ts";
+
 export default {
+  name: "Body",
+  data: () => ({
+    isValid: true,
+    isProcess: false,
+    keywordRules: [(value: string) => !!value || "Kata Kunci Harus diisi"],
+    keyword: "",
+    wikidata: [] as any[],
+    kbbiTitle: "",
+    kbbiArti: [] as any[]
+  }),
   methods: {
-    callIstilah: () => {
-      console.log("Triger");
+    callIstilah(): void {
+      if (this.isValid) {
+        this.isProcess = true;
+
+        //get from wikipedia
+        istilahService
+          .getwiki(this.keyword)
+          .then(result => {
+            this.wikidata = result.data.slice(0, 5);
+            console.log(this.wikidata);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+
+        //get from kbbi
+        istilahService.getKBBI(this.keyword).then(result => {
+          this.kbbiTitle = result.title;
+          this.kbbiArti = result.data.arti;
+          console.log(this.kbbiArti);
+        });
+      }
     }
   }
 };
